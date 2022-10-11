d3d5dcb5c392cab35b18109db9c0df5a01579e9a
kalebdaniel <kabby11567@gmail.com>
2022-10-10 22:06:26 -0400

    add pivot table

--- Python4Excel/Week 5/tiktokdataset.py
+++ Python4Excel/Week 5/tiktokdataset.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
-data = pd.read_excel("TikTok_songs_2022.csv")
+data = pd.read_csv("TikTok_songs_2022.csv")
 
+pd.pivot_table(data, index="track_information", columns='artist_information', values='fact_information')
+
+print(pivot)
+print(df)
+
+df(head)
 df = pd.DataFrame(data)
 print(df)
 
@@ -13,3 +19,14 @@ print(df)
 print(df.loc[18])
 
 print(df)
+
+df.groupby(["TikTok songs 2022"]).mean()
+pivot = pd.pivot_table(data,
+                 index="track_name", columns='artist_name',
+                 values="album",
+                 aggfunc='mean')
+print(pivot)
+
+print(pd.melt(pivot.iloc[:-1,:-1].reset_index(),
+        id_vars="track",
+        value_name="TikTok songs 2022"))
